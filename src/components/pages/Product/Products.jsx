600bba9d83c26fec063a94ace7eeb76ffb31b25a
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-10-12 20:28:27 +0300

    commit

--- src/components/pages/Product/Products.jsx
+++ src/components/pages/Product/Products.jsx
@@ -1,30 +1,37 @@
 import"./products.css"
-import React, { useContext,useState } from 'react'
+import React, { useContext,useState,useEffect } from 'react'
 import { Product } from '../../../service/productItems'
 import ProductsCard from '../ProductsCard/ProductsCard'
 import { cartContext } from "../../../context/CartContext"
 const Products = () => {
   const {cart,setCart}=useContext(cartContext)
-const [search,setSearch]=useState()
+const [search,setSearch]=useState("")
+// const [productHolder,setProductHolder]=useState([])
 
 const serchInput=(e)=>{
 setSearch(e.target.value)
 }
 
-const searchByName=()=>{
 
-}
+// useEffect(()=>{
+//   setProductHolder(Product.items)
+//   sercByName()
+// },[])
+// useEffect(()=>{
+//   sercByName()
+// },[search])
+// console.log(productHolder)
   return (
-    <div>
-      <div>
-      <div class="input-group rounded">
+    <div className="showCase">
+      <div className="  d-flex justify-content-center mt-5">
+      <div className="input-group rounded w-25 ">
   <input type="search" onChange={(e)=>serchInput(e)} class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-  <button>serch</button>
+  <button style={{background:"#FFE484",borderBlockColor:"#563D7C",color:"black"}}>search</button>
 </div>
       </div>
     <div className='productsItemCards mt-5 container-fluid d-flex justify-content-center '>
       <div className="row justify-content-center">
-      {Product.items.map((items)=>
+      {Product.items.filter((items)=>items.name.toLowerCase().includes(search.toLowerCase())).map((items)=>
     
     <ProductsCard items={items} cart={cart} setCart={setCart}/>
        )}
