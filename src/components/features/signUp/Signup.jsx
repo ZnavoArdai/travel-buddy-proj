8396991037bc18ce429fccd08e6f9d1a31965221
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-10-15 14:51:21 +0300

    c

--- src/components/features/signUp/Signup.jsx
+++ src/components/features/signUp/Signup.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
 function Signup() {
   const [showSignup, setShowSignup] = useState(false);
+  const handleShowSignup = () => setShowSignup(true);
+  const [nameSignup, setNameSignup] = useState("");
+  const [lastNameSignup, setLastNameSignup] = useState("");
+  const [photoSignup, setPhotoSignup] = useState("");
+  const [emailSignup, setEmailSignup] = useState("");
+  const [passwordSignup, setPasswordSignup] = useState("");
+  const [secPasswordSignup, setSecPasswordSignup] = useState("");
+  const [formEror,setFormEror]=useState({})
+  const [isSubmit,setIsSubmit]=useState(false)
+
+
+
   const handleCloseSignup = () => {
     setShowSignup(false);
 
   };
+
+
+
   const handleSignup = () => {
    localStorage.setItem('nameSignup', `${nameSignup}`);
    localStorage.setItem('emailSignup', `${emailSignup}`);
@@ -17,18 +32,50 @@ function Signup() {
    localStorage.setItem('lastNameSignup', `${lastNameSignup}`);
    localStorage.setItem('photoSignup', `${photoSignup}`);
 
-
-
+   setFormEror(validate())
+   setIsSubmit(true);
+   if(Object.keys(formEror).length==0&&isSubmit){
    setShowSignup(false);
-  };
-  const handleShowSignup = () => setShowSignup(true);
 
-  const [nameSignup, setNameSignup] = useState("");
-  const [lastNameSignup, setLastNameSignup] = useState("");
-  const [photoSignup, setPhotoSignup] = useState("");
-  const [emailSignup, setEmailSignup] = useState("");
-  const [passwordSignup, setPasswordSignup] = useState("");
-  const [secPasswordSignup, setSecPasswordSignup] = useState("");
+  }
+
+   
+  };
+
+  const validate=()=>{
+    const eror={};
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+    if(!nameSignup){
+      eror.userName="name required"
+    }
+    if(!lastNameSignup){
+      eror.userLastName="name required"
+    }
+    if(!emailSignup){
+      eror.userEmail="email required"
+    }
+    else if(!regex.test(emailSignup)){
+      eror.userEmail="not valid"
+    }
+    if(!passwordSignup){
+      eror.userPassword="password required"
+    }
+    if(!secPasswordSignup){
+      eror.userPasswordTwo="second password required"
+    }
+    else if(passwordSignup!==secPasswordSignup){
+      eror.userPasswordTwo="password not equals"
+    }
+    return eror;
+  }
+
+  useEffect(()=>{
+    console.log(formEror)
+if(Object.keys(formEror).length==0&&isSubmit){
+}
+  },[formEror])
+
+
 
   const handleNameSignup = (e) => {
     setNameSignup(e.target.value);
@@ -69,9 +116,9 @@ function Signup() {
                 placeholder="enter name"
                 autoFocus
                 required
-                pattern="[a-zA-Z0-9]+"
                 onChange={(e) => handleNameSignup(e)}
               />
+              <p style={{color:"red"}}>{formEror.userName}</p>
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputLastnameSignup">
               <Form.Label>Last Name</Form.Label>
@@ -82,6 +129,8 @@ function Signup() {
                 required
                 onChange={(e) => handleLastNameSignup(e)}
               />
+            <p style={{color:"red"}}>{formEror.userLastName}</p>
+
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputPhotoSignup">
               <Form.Label>Last Name</Form.Label>
@@ -91,6 +140,7 @@ function Signup() {
                 autoFocus
                 onChange={(e) => handlePhotoSignup(e)}
               />
+              
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputemailSignup">
               <Form.Label>Email Address</Form.Label>
@@ -101,6 +151,8 @@ function Signup() {
                 required
                 onChange={(e) => handleEmailSignup(e)}
               />
+            <p style={{color:"red"}}>{formEror.userEmail}</p>
+
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputpasswordSignup">
               <Form.Label>Password</Form.Label>
@@ -111,6 +163,8 @@ function Signup() {
                 required
                 onChange={(e) => handlePasswordSignup(e)}
               />
+             <p style={{color:"red"}}>{formEror.userPassword}</p>
+
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputSecpasswordSignup">
               <Form.Label>Password</Form.Label>
@@ -121,6 +175,8 @@ function Signup() {
                 required
                 onChange={(e) => handleSecPasswordSignup(e)}
               />
+             <p style={{color:"red"}}>{formEror.userPasswordTwo}</p>
+
             </Form.Group>
           </Form>
         </Modal.Body>
